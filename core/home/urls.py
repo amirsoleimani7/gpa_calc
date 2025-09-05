310b016a6144337535c6b4c221947a870591800a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-09-05 19:34:57 +0330

    custom loging and register page added

--- core/home/urls.py
+++ core/home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("" , views.home , name='home')
+
+    path('login/' , views.login_page , name='login') ,
+    path('logout/' , views.custom_logout ,name='logout') , 
+    path('register/' , views.register_page  , name='register')
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,5 +1,57 @@
 from django.shortcuts import render , redirect , HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login , logout
+
+from django.contrib import messages
+
+
 
 def home(request):
     return HttpResponse("sup!")
 
+
+def login_page(request):
+    if request.method == 'POST':
+        try: 
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user_obj = User.objects.filter(username=username)
+            if not user_obj.exists():
+                messages.error(request , 'username not found')
+                return redirect('/accounts/login/')
+            user_obj = authenticate(username=username , password=password)
+            if user_obj :
+                login(request , user_obj)
+                return redirect('receipts')
+            messages.error(request , "wrong password")
+            return redirect('/accounts/login')
+        except Exception as e:
+            messages.error(request , "something went wrong")
+            print(f"the error is : {e}")
+            return redirect("/accounts/login/")
+    return render(request , "login.html")
+
+
+
+def register_page(request):
+    if request.method == 'POST':
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user_obj = User.objects.filter(username=username)
+            if user_obj.exists():
+                messages.error(request , 'username is taken')
+                return redirect('/accounts/register/')
+            user_obj = User.objects.create(username=username)
+            user_obj.set_password(password)
+            user_obj.save()
+            messages.success(request , 'account created!')
+            return redirect("/accounts/login/")
+        except Exception as e:
+            messages.error(request , "something went wrong")
+            return redirect("/accounts/register/")
+    return render(request , 'register.html')
+
+def custom_logout(requets):
+    logout(requets)
+    return redirect('login')
