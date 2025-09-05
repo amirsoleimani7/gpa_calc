234ae3b9cd082171306c6306c9f48904207261ee
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-09-05 11:16:58 +0330

    login page added/ redrict to some other page added as well

--- core/home/views.py
+++ core/home/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render ,HttpResponse
+from django.shortcuts import render , redirect , HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login
+
+
+
 
 def home(request):
     return HttpResponse("sup!")
 
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
 
+    user = authenticate(request, username=username , password=password)
+    
+    if user is not None:
+        login(request , user) # user's id saved to the session of the request        
+        return redirect('home')
+    else:
+        return HttpResponse("wrong info")
