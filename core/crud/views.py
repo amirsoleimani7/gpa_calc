2592c6fef165fd54e595dc2e70a9071d828731f0
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-09-07 12:40:11 +0330

    main_page and search works

--- core/crud/views.py
+++ core/crud/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render ,HttpResponse
+from django.shortcuts import render , redirect , get_object_or_404 , HttpResponse
+from .models import Recipe
+
+
 
 def main_page(request):
-    return HttpResponse('this is the crud page')
+    
+    all_reciptes = Recipe.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        if request.POST.get('search'):
+            search_item = request.POST.get('search')
+            print(f"the search item is : {search_item}")
+            all_reciptes = Recipe.objects.filter(recipe_name__icontains=search_item)
+
+    
+    context = {
+        'all_reciptes' : all_reciptes
+    }
+    
+    return render(request , 'main_page_reciptes.html' , context)
+
+
+    
+
+
+
+
+
+
